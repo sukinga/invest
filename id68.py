4ffa845e77544f4f3534aa179bdf6609abf1adee
sukinga <1365804962@qq.com>
2016-01-11 17:19:43 +0800

    完成前海金融的扫描

--- id68.py
+++ id68.py
@@ -2,21 +2,36 @@
 __author__ = 'lixin'
 
 import httplib2
-import json
 from bs4 import BeautifulSoup
-import time
+import mail139
 
-h = httplib2.Http("")
+h = httplib2.Http("",timeout=15)
 transfer_url = 'http://www.id68.cn/transfer'
 resp, content = h.request(transfer_url,"GET",
                           headers={'user-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36'})
 soup = BeautifulSoup(content,"html.parser",from_encoding="utf-8")
 
+new_list = []
 for i in soup.find_all('ul','ideal_con')[1].find_all('li'):
-    #print bool(i.getText().encode('utf8').count('标满'))
-    #x = i.find_all('a')[0].encode('utf8')
-    #print type(x)
-    #x.find(start='/transfer/',end='.html')
-    #print i.find_all('href')
-    xx = 'afds'
-    xx.find(start='/transfer/',end='.html')
+    item = i.find_all('span')
+    id = item[0].encode('utf8').split('/transfer/')[1].split('.html')[0]
+    mouth = item[1].encode('utf8').split('">')[1].split('</')[0].strip()
+    rate = item[2].encode('utf8').split('">')[1].split('</')[0]
+    count = item[5].encode('utf8').split('">')[1].split('</')[0]
+    if(len(i.find_all('input'))==1):
+        new_list.append([int(id),mouth,rate,count])
+
+old_list = []
+file_object = open('id68.txt')
+for i in file_object:
+    old_list.append(int(i))
+file_object.close()
+for temp in new_list:
+    if temp[0] not in old_list:
+        title = '编号:'+str(temp[0])+';期限:'+temp[1]+';利率:'+temp[2]+';额度:'+temp[3]
+        if(mail139.semd_mail('13581798781@139.com',title,'') and mail139.semd_mail('13810090086@139.com',title,'')):
+        #if(mail139.semd_mail('13581798781@139.com',title,'')):
+            file_object = open('id68.txt','a')
+            file_object.write('\n')
+            file_object.write(str(temp[0]))
+            file_object.close()
---
+++ mail139.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+__author__ = 'lixin'
+
+import smtplib
+import email
+import sys
+from email.mime.text import MIMEText
+
+#========================================
+#需要配置
+send_mail_host="mail.baofeng.com"      # 发送的smtp
+send_mail_user="lixin@baofeng.com"
+send_mail_user_name="前海理想金融"
+send_mail_pswd="Qwer1234"
+send_mail_postfix="baofeng.com"  #发邮件的域名
+
+#get_mail_user="13581798781@139.com"
+
+#以下不用配置=============================
+
+get_mail_postfix="139.com"
+get_mail_host="pop.139.com"
+
+
+#========================================
+def semd_mail(get_mail_user,sub,content):
+    '''
+    sub:主题
+    content:内容
+    send_mail("xxxxx@xxx.xxx","主题","内容")
+    '''
+    send_mail_address=send_mail_user_name+"<"+send_mail_user+"@"+send_mail_postfix+">"
+    msg=email.mime.text.MIMEText(content)
+    msg['Subject']=sub
+    msg['From']=send_mail_address
+    msg['to']=to_adress="139SMSserver<"+get_mail_user+"@"+get_mail_postfix+">"
+    try:
+        stp = smtplib.SMTP()
+        stp.connect(send_mail_host)
+        stp.login(send_mail_user,send_mail_pswd)
+        stp.sendmail(send_mail_address, to_adress, msg.as_string())
+        stp.close()
+        return True
+    except Exception, e:
+        print str(e)
+        return False
+
+'''
+if __name__ == '__main__':
+
+    if semd_mail('13581798781@139.com','A',''):
+        print "发送成功"
+    else:
+        print '发送失败'
+'''
